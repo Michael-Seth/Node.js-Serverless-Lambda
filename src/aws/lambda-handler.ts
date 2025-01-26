f416f276a9b1c2144fe47242fcd69d5aad78ee5c
Michael Seth <michaelseth78@gmail.com>
2025-01-26 21:49:34 +0100

    009

--- src/aws/lambda-handler.ts
+++ src/aws/lambda-handler.ts
@@ -1,42 +1,48 @@
-"use strict"
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, Context, Handler } from "aws-lambda"
-import awsServerlessExpress from "aws-serverless-express"
+// "use strict"
+// import { APIGatewayProxyEvent, APIGatewayProxyHandler, Context, Handler } from "aws-lambda"
+// import awsServerlessExpress from "aws-serverless-express"
+// import app from "../app"
+// import { getLambdaEventSource } from "./lambda.event.source"
+
+// const server = awsServerlessExpress.createServer(app)
+// const expressApp: APIGatewayProxyHandler = (event: APIGatewayProxyEvent, context: Context) => {
+//   awsServerlessExpress.proxy(server, event, context)
+// }
+
+// const functionUrl: Handler = (event: APIGatewayProxyEvent, context: Context) => {
+//   console.log(getLambdaEventSource(event), "EVENT_SOURCE_F_URL")
+
+//   awsServerlessExpress.proxy(server, event, context)
+// }
+
+// const router = (source: ReturnType<typeof getLambdaEventSource>, event: any, context: any, callback: any) => {
+//   console.info("Event-Source:", source)
+
+//   switch (source) {
+//     case "isApiGateway":
+//       return expressApp(event, context, callback)
+//     case "isFuntionUrl":
+//       return functionUrl(event, context, callback)
+//     default:
+//       console.error("Unsupported event source", event)
+//   }
+// }
+
+// const handler: Handler = (event: APIGatewayProxyEvent | any, context: Context, callback: any) => {
+//   const source = getLambdaEventSource(event)
+
+//   try {
+//     return router(source, event, context, callback)
+//   } finally {
+//   }
+// }
+
+// export = {
+//   handler,
+// }
+
+import serverless from "serverless-http"
+
 import app from "../app"
-import { getLambdaEventSource } from "./lambda.event.source"
 
-const server = awsServerlessExpress.createServer(app)
-const expressApp: APIGatewayProxyHandler = (event: APIGatewayProxyEvent, context: Context) => {
-  awsServerlessExpress.proxy(server, event, context)
-}
-
-const functionUrl: Handler = (event: APIGatewayProxyEvent, context: Context) => {
-  console.log(getLambdaEventSource(event), "EVENT_SOURCE_F_URL")
-
-  awsServerlessExpress.proxy(server, event, context)
-}
-
-const router = (source: ReturnType<typeof getLambdaEventSource>, event: any, context: any, callback: any) => {
-  console.info("Event-Source:", source)
-
-  switch (source) {
-    case "isApiGateway":
-      return expressApp(event, context, callback)
-    case "isFuntionUrl":
-      return functionUrl(event, context, callback)
-    default:
-      console.error("Unsupported event source", event)
-  }
-}
-
-const handler: Handler = (event: APIGatewayProxyEvent | any, context: Context, callback: any) => {
-  const source = getLambdaEventSource(event)
-
-  try {
-    return router(source, event, context, callback)
-  } finally {
-  }
-}
-
-export = {
-  handler,
-}
+export const handler = serverless(app)
